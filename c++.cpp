a72290691706eac34783c53c9eb2e707c98f9d7e
Thariq Nugrohotomo <46335956+thariqnu-ifm@users.noreply.github.com>
2020-05-31 06:27:59 +0800

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,33 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<deque>
+using namespace std;
+typedef long long llong;
+int tcid=1;
+#define print(arr,separ) for(auto x:arr) = cout<<x<<separ;
+#define seq(i,a,b) for(i=a;i<=b;i++)
+stringstream cinln(){string s; if(getline(cin,s))return stringstream(s); exit(0);}
+#define cinln cinln()
+/*
+	tcid=1
+	cinln
+	llong
+	seq(iter,lo,hi)
+	print(arr,separ)
+*/
+int _next(){
+	if(tcid==1){
+	}else{
+	}
+	return 1;
+}
+void _mainloop(){
+}
+int main(){
+	while(_next()) {
+		_mainloop();
+		tcid++;
+	}
+}
