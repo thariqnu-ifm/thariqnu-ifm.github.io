3f2172f325e87ece72006e3a7c90473958abd201
Thariq Nugrohotomo <46335956+thariqnu-ifm@users.noreply.github.com>
2020-06-02 09:47:06 +0800

    Add files via upload

--- python3.py
+++ python3.py
@@ -6,26 +6,25 @@ null=None
 def perr(*args,**kwargs): print(*args,file=sys.stderr,**kwargs)
 def line():
 	ln=sys.stdin.readline().strip()
-#	perr(ln)
+	#perr(ln)
 	if ln=='': sys.exit()
 	return ln
+def lines(n): return [line() for i in range(n)]
 def split(ln=None): return (ln or line()).split()
 def parse(*parsers): return list(map((lambda p,w:p(w)),parsers,split()))
 def nums(o=None):
 	if o is not None and isinstance(o, str): o=split(o)
 	return list(map(int, o or split()))
 def num(): return int(line())
+#help("sys.modules")
 """
 perr(print) true false null
-num() nums(?) parse(..) split(?) line()
+num() nums(?) parse(..) split(?) lines(n) line()
 """
 # 
-# 11799 
 # 
-for tc in range(num()):
-	aa=nums()
-	aa.pop(0)
-	print("Case {}: {}".format(tc+1, max(aa)))
+# 
+
 #
 #
 #
