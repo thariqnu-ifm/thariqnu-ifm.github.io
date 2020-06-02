31c18755173b93f20cfda9b957c37c32bce7b675
Thariq Nugrohotomo <46335956+thariqnu-ifm@users.noreply.github.com>
2020-06-02 15:42:27 +0700

    nums(int)

--- python3.py
+++ python3.py
@@ -13,7 +13,9 @@ def lines(n): return [line() for i in range(n)]
 def split(ln=None): return (ln or line()).split()
 def parse(*parsers): return list(map((lambda p,w:p(w)),parsers,split()))
 def nums(o=None):
-	if o is not None and isinstance(o, str): o=split(o)
+	if o is not None:
+		if isinstance(o, int): return nums(lines(o))
+		if isinstance(o, str): o=split(o)
 	return list(map(int, o or split()))
 def num(): return int(line())
 #help("sys.modules")
