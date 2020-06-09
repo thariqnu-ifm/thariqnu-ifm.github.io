0a4c9b7c2119a367f6581813886319a64087c12f
Thariq Nugrohotomo <46335956+thariqnu-ifm@users.noreply.github.com>
2020-06-09 09:55:11 +0800

    Add files via upload

--- python3.py
+++ python3.py
@@ -10,6 +10,7 @@ import random
 true=True
 false=False
 null=None
+tcid=0
 def ceil(a,b):
 	ans=a//b
 	if a%b!=0: ans+=1
@@ -29,25 +30,35 @@ def nums(o=None):
 		elif isinstance(o, str): o=split(o)
 	return list(map(int, o or split()))
 def num(): return int(line())
+#
 #help("sys.modules")
 """
 ceil(a,b)
-perr(print) true false null
+perr(print) true false null tcid
 num() nums(?) parse(..) split(?) lines(n) line()
 """
-# 
-# 
+def solve(tcid=1):
+	a=nums()[1:]
+	bb=set()
+	for ii in range(len(a)-1):
+		ad=abs(a[ii+1] - a[ii])
+		if ad>0 and ad<len(a):
+			bb.add(ad)
+	ans=["Not jolly","Jolly"]
+	print(ans[len(bb)==len(a)-1])
 #
-
-#
-tcid=0
 while true:
 	tcid+=1
-	#
-	
-#
-#
-#
-#
-#
-#
+	solve(tcid)
+"""
+Sample Input
+4 1 4 2 3
+5 1 4 2 -1 6
+Sample Output
+Jolly
+Not jolly
+"""
+#	
+"""
+10038
+"""
