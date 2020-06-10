f099f09e7196301fa7d1c65a5d6529cee9c6e4b8
Thariq Nugrohotomo <46335956+thariqnu-ifm@users.noreply.github.com>
2020-06-10 15:18:06 +0700

    Update python3.py

--- python3.py
+++ python3.py
@@ -11,6 +11,8 @@ true=True
 false=False
 null=None
 tcid=0
+tcmax=99999999
+def sround(val,nd): return f'{val:.{nd}f}'
 def ceil(a,b):
 	ans=a//b
 	if a%b!=0: ans+=1
@@ -23,42 +25,34 @@ def line():
 	return ln
 def lines(n): return [line() for i in range(n)]
 def split(ln=None): return (ln or line()).split()
-def parse(*parsers): return list(map((lambda p,w:p(w)),parsers,split()))
 def nums(o=None):
 	if o is not None:
 		if isinstance(o, int): o=lines(o)
 		elif isinstance(o, str): o=split(o)
-	return list(map(int, o or split()))
-def num(): return int(line())
+	return list(map(eval, o or split()))
+def num(): return eval(line())
 #
 #help("sys.modules")
 """
-ceil(a,b)
-perr(print) true false null tcid
-num() nums(?) parse(..) split(?) lines(n) line()
+ceil(a,b) sround(val,nd) true false null
+num() nums(?) split(?) lines(n) line()
+perr(print) tcmax tcid
 """
+# {{{
+
+# }}}
 def solve(tcid=1):
-	a=nums()[1:]
-	bb=set()
-	for ii in range(len(a)-1):
-		ad=abs(a[ii+1] - a[ii])
-		if ad>0 and ad<len(a):
-			bb.add(ad)
-	ans=["Not jolly","Jolly"]
-	print(ans[len(bb)==len(a)-1])
-#
-while true:
+# {{{
+	exit()
+# }}}
+while tcid<tcmax:
 	tcid+=1
 	solve(tcid)
 """
-Sample Input
-4 1 4 2 3
-5 1 4 2 -1 6
-Sample Output
-Jolly
-Not jolly
+notes = .
+
 """
-#	
 """
-10038
+problem #id = .
+
 """
