03922872b4992bf8f10be9b5ec4b9c9b694fc3ca
Thariq Nugrohotomo <46335956+thariqnu-ifm@users.noreply.github.com>
2020-06-10 15:33:11 +0700

    Update c++.cpp

--- c++.cpp
+++ c++.cpp
@@ -11,23 +11,23 @@ int tcid=1;
 stringstream cinln(){string s; if(getline(cin,s))return stringstream(s); exit(0);}
 #define cinln cinln()
 /*
-	tcid=1
-	cinln
-	llong
-	seq(iter,lo,hi)
-	print(arr,separ)
+ tcid=1
+ cinln
+ llong
+ seq(iter,lo,hi)
+ print(arr,separ)
 */
 int _next(){
-	if(tcid==1){
-	}else{
-	}
-	return 1;
+ if(tcid==1){
+ }else{
+ }
+ return 1;
 }
 void _mainloop(){
 }
 int main(){
-	while(_next()) {
-		_mainloop();
-		tcid++;
-	}
+ while(_next()) {
+  _mainloop();
+  tcid++;
+ }
 }
