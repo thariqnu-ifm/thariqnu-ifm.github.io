714ce7c2c0e8706ec9484877d3da382a851f4b98
Thariq Nugrohotomo <46335956+thariqnu-ifm@users.noreply.github.com>
2020-06-27 11:03:15 +0700

    Create python2test.py

---
+++ python2test.py
@@ -0,0 +1,38 @@
+
+#
+print(sround(123.456789,2))
+perr(1)
+print(2)
+fast()
+perr(3)
+print(4)
+print(nums())
+print(nums(3))
+print(range)
+print(memoi)
+@memoi
+def fib(ii):
+    if ii<2: return 1
+    return fib(ii-1)+fib(ii-2)
+print(fib(99))
+def hello(tcid):
+    print("hello", tcid)
+loop(hello,9)
+print(list(reversed(range(9))))
+aa=arrays(1,9)
+print(aa)
+aa[4]=9
+print(aa)
+print(aa[4])
+print(aa[-1])
+print(type(aa))
+bb=arrays(2,3,3)
+print(bb)
+bb[1,1]=9
+print(bb)
+print(bb[1,1])
+print(type(bb))
+cc=arrays(3,2,2,2)
+print(cc)
+cc[0,1,0]=9
+print(cc)
